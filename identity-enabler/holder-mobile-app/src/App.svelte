6a52a2e7c954a0a579812f86c122ec7e1c075972
Alexey Sobolev <sobolev@mail.de>
2021-06-18 20:18:50 +0200

    App navigation and routing

--- identity-enabler/holder-mobile-app/src/App.svelte
+++ identity-enabler/holder-mobile-app/src/App.svelte
@@ -1,6 +1,8 @@
 <script>
 	import { Link, Router, Route } from 'svelte-routing';
+	import { onMount } from 'svelte';
 	import Home from './pages/Home.svelte';
+	import { ServiceFactory } from './factories/serviceFactory';
 	import CreateIdentity from './pages/CreateIdentity.svelte';
 	import CreateCredential from './pages/CreateCredential.svelte';
 	import CreatePresentation from './pages/CreatePresentation.svelte';
@@ -8,25 +10,64 @@
 	import Splash from './pages/Splash.svelte';
 	import Landing from './pages/Landing.svelte';
 	import Name from './pages/Name.svelte';
+	import Menu from './pages/Menu.svelte';
+	import DataMatrix from './pages/DataMatrix.svelte';
+	import Credential from './pages/Credential.svelte';
+
+
+	import { SPLASH_SCREEN_TIMEOUT } from './config';
+	import { hasSetupAccount } from './lib/store';
+	import Keychain from './lib/keychain';
+	import { IdentityService } from './services/identityService';
 
 	let url = window.location.pathname;
+	let displayHome = false;
+	let splash = true;
+
+	onMount(async () => {
+			setTimeout(async () => {
+				splash = false;
+			}, SPLASH_SCREEN_TIMEOUT);
+
+			if (!$hasSetupAccount) {
+					return Keychain.clear();
+			}
+
+			const identityService = ServiceFactory.get('identity');
+			const storedIdentity = await identityService.retrieveIdentity();
+
+			if (storedIdentity) {
+				console.log('Found identity', storedIdentity)
+				displayHome = true;
+			}
+
+			// retrieveCredentials($listOfCredentials.values).then((credentials) => {
+			// 		displayHome = true;
+			// 		storedCredentials.set(credentials);
+			// });
+	});
 </script>
 
 <main>
 	<Router url="{url}">
-		<!-- <nav>
-			<Link to="/">Home</Link>
-			<Link to="createIdentity">CreateIdentity</Link>
-			<Link to="createCredential">CreateCredential</Link>
-			<Link to="createPresentation">CreatePresentation</Link>
-			<Link to="verifyPresentation">VerifyPresentation</Link>
-		</nav> -->
 		<div>
-			<Route path="/" component="{Home}" />
+			{#if splash}
+				<Route path="/" component="{Splash}" />
+			{:else}
+				{#if !$hasSetupAccount}
+					<Route path="/" component="{Landing}" />
+				{:else if displayHome}
+					<Route path="/" component="{Home}" />
+				{/if}
+			{/if}
+			<Route path="/home" component="{Home}" />
+			<Route path="/menu" component="{Menu}" />
 			<Route path="/splash" component="{Splash}" />
 			<Route path="/landing" component="{Landing}" />
-			<Route path="/onboarding/name" component="{Name}" />
-
+			<Route path="/name" component="{Name}" />
+			<Route path="/credential" component="{Credential}" />
+			<Route path="/datamatrix" component="{DataMatrix}" />
+			
 			<Route path="/createIdentity" component="{CreateIdentity}" />
 			<Route path="/createCredential" component="{CreateCredential}" />
 			<Route path="/createPresentation" component="{CreatePresentation}" />
