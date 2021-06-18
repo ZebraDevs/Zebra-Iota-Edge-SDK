41754f48fa857f1a8bd5af30cd4d024242a390cf
Alexey Sobolev <sobolev@mail.de>
2021-06-18 19:18:03 +0200

    flatten object helper

--- identity-enabler/holder-mobile-app/src/lib/helpers.ts
+++ identity-enabler/holder-mobile-app/src/lib/helpers.ts
@@ -190,3 +190,32 @@ export function persistent<T>(key: string, initialValue: T, saveTransformation?:
 };
 
 export function generateRandomId(): string { return uuidv4(); }
+
+export function flattenObj(ob) {
+  
+    // The object which contains the
+    // final result
+    let result = {};
+  
+    // loop through the object "ob"
+    for (const i in ob) {
+  
+        // We check the type of the i using
+        // typeof() function and recursively
+        // call the function again
+        if ((typeof ob[i]) === 'object') {
+            const temp = flattenObj(ob[i]);
+            for (const j in temp) {
+  
+                // Store temp in result
+                result[i + '.' + j] = temp[j];
+            }
+        }
+  
+        // Else store ob[i] in result directly
+        else {
+            result[i] = ob[i];
+        }
+    }
+    return result;
+};
