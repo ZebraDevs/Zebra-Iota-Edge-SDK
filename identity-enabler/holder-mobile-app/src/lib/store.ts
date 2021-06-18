34ff6edbe5b0aa1b1095c35bea9a75d0c18508a4
Alexey Sobolev <sobolev@mail.de>
2021-06-18 20:19:05 +0200

    Storage helpers

--- identity-enabler/holder-mobile-app/src/lib/store.ts
+++ identity-enabler/holder-mobile-app/src/lib/store.ts
@@ -8,6 +8,35 @@ import type { IdentityService } from '../services/identityService';
 init();
 const identityService = ServiceFactory.get<IdentityService>('identity');
 
+export const updateStorage = async (key, value) => {
+    try {
+        let stored = {};
+        let updated = {};
+        if (localStorage.getItem(key)) {
+            stored = JSON.parse(await localStorage.getItem(key));
+            updated = {...stored, ...value};
+        } else {
+            updated = [value];
+        }
+        await localStorage.setItem(key, JSON.stringify(updated));
+        return;
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+export const getFromStorage = async (key) => {
+    try {
+        const json = localStorage.getItem(key);
+        if (json) {
+            return JSON.parse(json);
+        }
+        return null;
+    } catch (err) {
+        console.error(err);
+    }
+}
+
 /**
  * Determines if use has completed onboarding
  */
@@ -20,6 +49,15 @@ export const listOfCredentials = persistent<{ init: boolean; values: string[] }>
         values: [],
     },
     (value) => ({ ...value, init: false })
+);
+
+export const credentials = persistent<{ personal: string, health: string, blood: string }>(
+    'credentials',
+    {
+        personal: '',
+        health: '',
+        blood: ''
+    },
 );
 
 export const account = persistent<{ name: string } | null>('account', null);
@@ -50,25 +88,51 @@ export interface InternalCredentialDataModel {
 export const storedCredentials = writable<InternalCredentialDataModel[]>([]);
 
 storedCredentials.subscribe((value) => {
+    console.log('storedCredentials 1', value);
     listOfCredentials.update((prev) => {
+        console.log('storedCredentials 2', prev);
+
         if (prev.init) {
+            console.log('storedCredentials 3', prev.values);
+
             const idsToDelete = prev.values.filter((id) => !value.find((credential) => credential.id === id));
+            console.log('storedCredentials 4', idsToDelete);
+            
             idsToDelete.map((id) => identityService.removeCredential(id));
+            console.log('storedCredentials 44', idsToDelete);
+            console.log('storedCredentials 45', prev);
+            console.log('storedCredentials 46', value.map((credential) => credential.id));
+
             return { ...prev, values: value.map((credential) => credential.id) };
         }
+        console.log('storedCredentials 5', prev);
+
         return { ...prev, init: true };
     });
+    console.log('storedCredentials 6');
+
     value.map((credential) => {
+        console.log('storedCredentials 7', credential);
+
         if (!credential.enrichment) {
-            const enrichment = identityService.enrichCredential(credential.credentialDocument)
+            console.log('storedCredentials 8', credential.credentialDocument);
+            
+            const enrichment = identityService.enrichCredential(credential.credentialDocument);
+            console.log('storedCredentials 9', enrichment);
+            console.log('storedCredentials 99', storedCredentials);
+
             storedCredentials.update((prev) =>
-                prev.map((prevCredential) =>
-                    prevCredential.id === credential.id
+                prev.map((prevCredential) => {
+                    console.log('storedCredentials 10', prevCredential, credential);
+
+                    return prevCredential.id === credential.id
                         ? { ...prevCredential, enrichment }
                         : prevCredential
-                )
+                })
             );
         }
+        console.log('storedCredentials 11', credential);
+
         return identityService.storeCredential(credential.id, credential);
     });
 });
