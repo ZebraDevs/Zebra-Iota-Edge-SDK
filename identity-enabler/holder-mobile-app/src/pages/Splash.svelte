ac72bf50ebbfcc185d4060c4c4290753e93f4e64
Karim Kozman <kimo_emad@hotmail.com>
2021-06-29 08:49:05 +0200

    splash screen

--- identity-enabler/holder-mobile-app/src/pages/Splash.svelte
+++ identity-enabler/holder-mobile-app/src/pages/Splash.svelte
@@ -1,6 +1,6 @@
 <style>
     div {
-        background: grey;
+        background: black;
         display: flex;
         justify-content: center;
         flex-direction: column;
@@ -9,13 +9,24 @@
         width: 100%;
     }
 
-    img {
+    .zebra-logo {
         margin: 0;
         width: 24vw;
         height: 100vh;
     }
+
+    .iota-logo {
+        margin-bottom: 58px;
+        width: 411px;
+        height: 69px;
+        display: flex;
+        justify-content: flex-end;
+        flex-direction: column;
+        align-items: center;
+    }
 </style>
 
 <div>
-    <img src="../assets/logo.svg" alt="" />
+    <img class="zebra-logo" src="../assets/logo.svg" alt="" />
+    <img class="iota-logo" src="../assets/IOTA-logo.svg" alt="" />
 </div>
