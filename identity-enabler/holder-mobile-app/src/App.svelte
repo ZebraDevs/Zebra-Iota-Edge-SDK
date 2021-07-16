fa6588b416b06732ba6d6461ec85c495ab388add
Karim Kozman <kimo_emad@hotmail.com>
2021-07-16 10:52:50 +0200

    remove old splash screen

--- identity-enabler/holder-mobile-app/src/App.svelte
+++ identity-enabler/holder-mobile-app/src/App.svelte
@@ -1,12 +1,11 @@
 <script>
-	import { Link, Router, Route } from 'svelte-routing';
+	import { Router, Route } from 'svelte-routing';
 	import { onMount } from 'svelte';
 	import Home from './pages/Home.svelte';
 	import { ServiceFactory } from './factories/serviceFactory';
 	import CreateIdentity from './pages/CreateIdentity.svelte';
 	import CreateCredential from './pages/CreateCredential.svelte';
 	import CreatePresentation from './pages/CreatePresentation.svelte';
-	import Splash from './pages/Splash.svelte';
 	import Landing from './pages/Landing.svelte';
 	import Name from './pages/Name.svelte';
 	import Menu from './pages/Menu.svelte';
@@ -17,21 +16,13 @@
 	import Content from './components/modal/Content.svelte';
 	import Modal from './components/modal/Index.svelte';
 
-
-	import { SPLASH_SCREEN_TIMEOUT } from './config';
 	import { hasSetupAccount } from './lib/store';
 	import Keychain from './lib/keychain';
-	import { IdentityService } from './services/identityService';
 
 	let url = window.location.pathname;
 	let displayHome = false;
-	let splash = true;
 
 	onMount(async () => {
-			setTimeout(async () => {
-				splash = false;
-			}, SPLASH_SCREEN_TIMEOUT);
-
 			if (!$hasSetupAccount) {
 					return Keychain.clear();
 			}
@@ -50,18 +41,13 @@
 	<Router url="{url}">
 		
 		<div>
-			{#if splash}
-				<Route path="/" component="{Splash}" />
-			{:else}
-				{#if !$hasSetupAccount}
-					<Route path="/" component="{Landing}" />
-				{:else if displayHome}
-					<Route path="/" component="{Home}" />
-				{/if}
+			{#if !$hasSetupAccount}
+				<Route path="/" component="{Landing}" />
+			{:else if displayHome}
+				<Route path="/" component="{Home}" />
 			{/if}
 			<Route path="/home" component="{Home}" />
 			<Route path="/menu" component="{Menu}" />
-			<Route path="/splash" component="{Splash}" />
 			<Route path="/landing" component="{Landing}" />
 			<Route path="/name" component="{Name}" />
 			<Route path="/credential" component="{Credential}" />
