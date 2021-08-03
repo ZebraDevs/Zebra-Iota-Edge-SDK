602e185bbd60a019e0d93a7098b40f58d4399089
Karim Kozman <kimo_emad@hotmail.com>
2021-08-03 19:03:05 +0200

    remove unused variable

--- identity-enabler/verifier-mobile-app/src/config.ts
+++ identity-enabler/verifier-mobile-app/src/config.ts
@@ -8,6 +8,4 @@ export const DEFAULT_LANGUAGE = 'English';
 
 export const DEFAULT_LOCALE = 'en';
 
-export const RANDOM_USER_DATA_API_URL = 'https://randomuser.me/api/?nat=us,au,ca,ie,gb';
-
 export const TUTORIAL_BASE_URL = 'https://jmcanterafonseca-iota.github.io/zebra-iota-edge-sdk';
