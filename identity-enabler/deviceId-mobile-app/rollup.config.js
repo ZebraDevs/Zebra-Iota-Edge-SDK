1ad8f55be2689f0d9a3dde4437451274400d67cb
Severin Skillman <severin.skillman@iota.org>
2021-11-26 13:42:23 +0100

    addressed install and build warnings

--- identity-enabler/deviceId-mobile-app/rollup.config.js
+++ identity-enabler/deviceId-mobile-app/rollup.config.js
@@ -37,7 +37,7 @@ function serve() {
 export default {
 	input: 'src/main.ts',
 	output: {
-		sourcemap: true,
+		sourcemap: !production,
 		format: 'iife',
 		name: 'app',
 		file: 'public/build/bundle.js'
@@ -102,5 +102,21 @@ export default {
 	],
 	watch: {
 		clearScreen: false
+	},
+	moduleContext: (id) => {
+		// In order to match native module behaviour, Rollup sets `this`
+		// as `undefined` at the top level of modules. Rollup also outputs
+		// a warning if a module tries to access `this` at the top level.
+		
+		// The following modules use `this` at the top level and expect it
+		// to be the global `window` object (runs in a browser), so we tell
+		// Rollup to set `this = window` for these modules.
+		const thisAsWindowForModules = [
+		  'node_modules/@zxing/library/esm',
+		];
+	  
+		if (thisAsWindowForModules.some(id_ => id.trimRight().includes(id_))) {
+		  return 'window';
+		}
 	}
 };
