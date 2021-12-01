ec93aed5ccd420539ea81bc0791eaa7a2a886db2
Severin Skillman <severin.skillman@iota.org>
2021-12-01 16:22:16 +0100

    fix permanode url

--- identity-enabler/holder-mobile-app/src/config.ts
+++ identity-enabler/holder-mobile-app/src/config.ts
@@ -4,7 +4,7 @@ export const API_URL = 'http://localhost:4000';
 
 export const IOTA_NODE_URL = 'https://chrysalis-nodes.iota.org';
 
-export const IOTA_PERMANODE_URL = 'https://chrysalis-chronicle.iota.org/api/mainnet';
+export const IOTA_PERMANODE_URL = 'https://chrysalis-chronicle.iota.org/api/mainnet/';
 
 export const DEFAULT_LANGUAGE = 'English';
 
