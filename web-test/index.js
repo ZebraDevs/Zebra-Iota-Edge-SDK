d836448b92bf3c5c9d31f196b47ceecf059bc3cc
Severin Skillman <severin.skillman@iota.org>
2021-12-01 09:28:10 +0100

    added web-test page

---
+++ web-test/index.js
@@ -0,0 +1,24 @@
+init()
+.then(() => console.log('Initialized'))
+.catch(e => {
+    console.error(e);
+    alert(`Error during initialization: ${e.message}`);
+});
+
+
+async function init() {
+  const videoEl = document.getElementById("video");
+
+  if (!navigator.mediaDevices?.getUserMedia) {
+    throw new Error("Browser camera access not supported.");
+  }
+
+  videoEl.srcObject = await navigator.mediaDevices.getUserMedia({
+    audio: false,
+    video: {
+      facingMode: { ideal: "environment" },
+      width: { ideal: 4096 },
+      height: { ideal: 2160 },
+    },
+  });
+}
