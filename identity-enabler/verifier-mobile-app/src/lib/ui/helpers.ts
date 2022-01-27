f20bce48dd21f4e133e08335e9e721556da1d784
Severin Skillman <severin.skillman@iota.org>
2022-01-27 09:48:31 +0100

    length checks

--- identity-enabler/verifier-mobile-app/src/lib/ui/helpers.ts
+++ identity-enabler/verifier-mobile-app/src/lib/ui/helpers.ts
@@ -52,7 +52,7 @@ export function flattenCredential(object: Record<string, any>): [string, string]
         return [];
     }
 
-    if (!object.credentialSubject || !Array.isArray(object.type) || !object.type.includes("VerifiableCredential")) {
+    if (!object.credentialSubject || !Array.isArray(object.type) || !object.type.length || !object.type.includes("VerifiableCredential")) {
         // Arbitrary objects get flattened
         console.log(flattenObj(object));
         return Object.entries(flattenObj(object));
