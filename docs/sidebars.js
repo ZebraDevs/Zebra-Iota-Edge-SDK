b274cccc72fea652e9ab7d9c5dd8e8f38d84296d
Dr-Electron <dr-electr0n@protonmail.com>
2022-01-30 23:42:50 +0100

    Add docusaurus sidebar

---
+++ docs/sidebars.js
@@ -0,0 +1,19 @@
+/**
+ * * Creating a sidebar enables you to:
+ - create an ordered group of docs
+ - render a sidebar for each doc of that group
+ - provide next/previous navigation
+
+ The sidebars can be generated from the filesystem, or explicitly defined here.
+
+ Create as many sidebars as you want.
+ */
+
+module.exports = {
+    docs: [
+        "tutorials/101/zebra-iota-edge-sdk-101-tutorial",
+        "tutorials/102/zebra-iota-edge-sdk-102-tutorial",
+        "tutorials/103/zebra-iota-edge-sdk-103-tutorial",
+        "tutorials/104/zebra-iota-edge-sdk-104-tutorial",
+    ],
+};
